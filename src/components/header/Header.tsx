da79095d6ba82f64fa407e1a03b12032ce6f27ad
joaovmour4 <joaov.mour@unifesspa.edu.br>
2024-03-05 10:33:51 -0300

    Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import logoImg from '../../img/MG logo x7 - svg - ORIGINAL 1.png'
+import logoImg from '../../img/MG logo x7 - svg - ORIGINAL.svg'
+import headerButton from '../headerButton/HeaderButton';
 
 function Header() {
   return (
-    <div>
-      <img className='object-contain h-20 w-30' src={logoImg} alt="Logo MG" />
-      <div>
-        <h1 className="flex justify-center text-3xl font-bold underline text-red-600">
-            Texto de teste para ajustes de tamanho
-        </h1>
-        <h1 className="flex justify-center text-3xl font-bold underline text-red-600">
-            Simple React Typescript Tailwind Sample
-        </h1>
+    <div className='flex flex-row relative bg-blue'>
+      <img className='flex-none h-auto w-36' src={logoImg} alt="Logo MG" />
+      <div className='flex flex-1 flex-row justify-end items-center justify-items-center pr-7'>
+        {headerButton('Home')}
+        {headerButton('Meus Certificados')}
+        {headerButton('E-Mails')}
       </div>
     </div>
   );
---
+++ src/components/headerButton/HeaderButton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function headerButton(name: string){
+    return(
+        <a href="https://google.com" className='px-4'>{name}</a>
+    )
+}
+
+export default headerButton
