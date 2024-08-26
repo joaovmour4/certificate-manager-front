c8df2f62e90acbcd8e846ee266683dd3499f0880
joaovmour4 <joaov.mour@unifesspa.edu.br>
2024-08-26 08:48:14 -0300

    docker-compose

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: `http://localhost:${process.env.API_PORT}`,
+  baseURL: `http://${process.env.API_URL}:${process.env.API_PORT}`,
 });
 
 api.interceptors.request.use(
