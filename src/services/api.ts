03f0765802c47cdba7b4f6c5fa2cbe0311e79631
joaovmour4 <joaov.mour@unifesspa.edu.br>
2024-08-27 09:09:22 -0300

    alterando nome .env vars

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 // import 'dotenv'
 
 const api = axios.create({
-  baseURL: `http://${process.env.API_URL}:${process.env.API_PORT}`,
+  baseURL: `http://${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}`,
 });
 
 api.interceptors.request.use(
