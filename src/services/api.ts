c69ff849696adca209249ff508b0ef81e8d92431
joaovmour4 <joaov.mour@unifesspa.edu.br>
2024-08-27 09:17:06 -0300

    alterando nome .env vars

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,6 +5,8 @@ const api = axios.create({
   baseURL: `http://${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}`,
 });
 
+console.log(`${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}`)
+
 api.interceptors.request.use(
   config => {
     const token = localStorage.getItem('userToken')
